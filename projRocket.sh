f0dd850d52eb259525b233e3eb4cd5ddf7c489f7
Angel Medrano <asmedrano@gmail.com>
2012-09-23 16:17:37 -0400

    Initial commit of WP Bootstrap

--- projRocket.sh
+++ projRocket.sh
@@ -30,9 +30,28 @@ done
 
 
 start_wp_project (){
-	echo "Starting Wordpress project, codename: $PROJ_NAME"
-	mkdir "$DIR/$PROJ_NAME"
 
+	echo "Starting Wordpress project, codename: $PROJ_NAME"
+
+	if [ ! -d "$DIR/$PROJ_NAME" ];
+		then
+			mkdir "$DIR/$PROJ_NAME"
+	fi
+
+	# its really up to each project handler to take it from here.
+	# So what do we need:
+	
+	# Wordpress installation lets get it from Github
+	git clone git://github.com/WordPress/WordPress.git "$DIR/$PROJ_NAME/wordpress"
+	
+	# a bootstrapped Theme directory, we'll ln -s it in wordpress content. I like using HTML5 Boilerplate so lets use this thing
+	git clone git://github.com/asmedrano/html5-boilerplate-for-wordpress.git "$DIR/$PROJ_NAME/$PROJ_NAME-theme"
+	
+	# get rid of all the .git files so this is really just a bunch of files. TODO: is there an actual way to do this?
+	rm -rf "$DIR/$PROJ_NAME/$PROJ_NAME-theme/.git"
+
+	# now we need to ln -s the theme into wordpress content, cause we dont want to check in the wordpress install into github.
+	ln -s `readlink -e $DIR/$PROJ_NAME/$PROJ_NAME-theme` "$DIR/$PROJ_NAME/wordpress/wp-content/themes/$PROJ_NAME-theme"	
 }
 
 
